4986fb6d379575a0d70e964ad89a17ba02c881f8
drjam2005 <jamesallanmdr2005@gmail.com>
2025-05-06 10:21:16 +0800

    Person Set

--- assets/objects.cpp
+++ assets/objects.cpp
@@ -1,6 +1,18 @@
-// #include <string>
+#include <string>
 
 class Person{
+    private:
+	int ID;
+	std::string name;
+	int age;
+	char sex;
+    public:
+	Person(int givenID, std::string givenName, int givenAge, char givenSex){
+	    ID = givenID;
+	    name = givenName;
+	    age = givenAge;
+	    sex = givenSex;
+	}
     // TODO
 };
 
